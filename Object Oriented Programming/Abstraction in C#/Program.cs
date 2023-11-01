8a08762073138e07a1ab09f5d6be7ea63059b10a
Cuong <cuonghustmi1@gmail.com>
2023-11-01 21:29:04 +0700

    Demine
    1/11/2023

--- Object Oriented Programming/Abstraction in C#/Program.cs
+++ Object Oriented Programming/Abstraction in C#/Program.cs
@@ -301,4 +301,61 @@
     //}
     #endregion
     #endregion
+
+    #region Example to understand Abstract class and Abstract methods in C#
+    public abstract class AbsParent
+    {
+        public void Add(int x, int y)
+        {
+            Console.WriteLine($"Addition of {x} and {y} is : {x + y}");
+        }
+
+        public void Sub(int x, int y)
+        {
+            Console.WriteLine($"Subtraction of {x} and {y} is : {x - y}");
+        }
+
+        public abstract void Mul(int x, int y);
+        public abstract void Div(int x, int y);
+    }
+
+    public class AbsChild : AbsParent
+    {
+        public override void Mul(int x, int y)
+        {
+            Console.WriteLine($"Multiplication of {x} and {y} is : {x * y}");
+        }
+
+        public override void Div(int x, int y)
+        {
+            Console.WriteLine($"Division of {x} and {y} is : {x / y}");
+        }
+    }
+
+    class MainClass
+    {
+        static void Main(string[] args)
+        {
+            // Error, we can't create an instance of an abstract class
+            //AbsParent parent = new AbsParent();
+
+            AbsChild absChild = new AbsChild();
+
+            // Creating abstract class reference pointing to child class object
+            AbsParent absParent = absChild;
+
+            absChild.Sub(1, 2);
+            absChild.Mul(1, 2);
+            absChild.Div(1, 2);
+            absChild.Add(1, 2);
+
+            Console.WriteLine();
+
+            absParent.Add(5, 10);
+            absParent.Sub(5, 10);
+            absParent.Div(5, 10);
+            absParent.Mul(5, 10);
+        }
+    }
+    #endregion
 }
