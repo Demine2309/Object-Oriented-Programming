2d44eb3015a9ea4be42ab0f766878f02da036cd7
Cuong <cuonghustmi1@gmail.com>
2023-11-03 01:58:32 +0700

    Demine
    3/11/2023

--- Object Oriented Programming/Abstraction in C#/Program.cs
+++ Object Oriented Programming/Abstraction in C#/Program.cs
@@ -303,58 +303,190 @@
     #endregion
 
     #region Example to understand Abstract class and Abstract methods in C#
-    public abstract class AbsParent
+    //public abstract class AbsParent
+    //{
+    //    public void Add(int x, int y)
+    //    {
+    //        Console.WriteLine($"Addition of {x} and {y} is : {x + y}");
+    //    }
+
+    //    public void Sub(int x, int y)
+    //    {
+    //        Console.WriteLine($"Subtraction of {x} and {y} is : {x - y}");
+    //    }
+
+    //    public abstract void Mul(int x, int y);
+    //    public abstract void Div(int x, int y);
+    //}
+
+    //public class AbsChild : AbsParent
+    //{
+    //    public override void Mul(int x, int y)
+    //    {
+    //        Console.WriteLine($"Multiplication of {x} and {y} is : {x * y}");
+    //    }
+
+    //    public override void Div(int x, int y)
+    //    {
+    //        Console.WriteLine($"Division of {x} and {y} is : {x / y}");
+    //    }
+    //}
+
+    //class MainClass
+    //{
+    //    static void Main(string[] args)
+    //    {
+    //        // Error, we can't create an instance of an abstract class
+    //        //AbsParent parent = new AbsParent();
+
+    //        AbsChild absChild = new AbsChild();
+
+    //        // Creating abstract class reference pointing to child class object
+    //        AbsParent absParent = absChild;
+
+    //        absChild.Sub(1, 2);
+    //        absChild.Mul(1, 2);
+    //        absChild.Div(1, 2);
+    //        absChild.Add(1, 2);
+
+    //        Console.WriteLine();
+
+    //        absParent.Add(5, 10);
+    //        absParent.Sub(5, 10);
+    //        absParent.Div(5, 10);
+    //        absParent.Mul(5, 10);
+    //    }
+    //}
+    #endregion
+
+    #region How to Use Abstract Classes and Abstract Methods in C# Application
+    //public abstract class Shape
+    //{
+    //    public double Height;
+    //    public double Width;
+    //    public double Radius;
+    //    public const float PI = 3.14f;
+
+    //    public abstract double GetArea();
+    //}
+
+    //public class Rectangle : Shape
+    //{
+    //    public Rectangle(double Height, double Width)
+    //    {
+    //        this.Height = Height;
+    //        this.Width = Width;
+    //    }
+
+    //    public override double GetArea()
+    //    {
+    //        return Width * Height;
+    //    }
+    //}
+
+    //public class Circle : Shape
+    //{
+    //    public Circle(double Radius)
+    //    {
+    //        this.Radius = Radius;
+    //    }
+
+    //    public override double GetArea()
+    //    {
+    //        return PI * Radius * Radius;
+    //    }
+    //}
+
+    //public class Triangle : Shape
+    //{
+    //    public Triangle(double Height, double Width)
+    //    {
+    //        this.Height = Height;
+    //        this.Width = Width;
+    //    }
+
+    //    public override double GetArea()
+    //    {
+    //        return (Width * Height) / 2;
+    //    }
+    //}
+
+    //public class Cone : Shape
+    //{
+    //    public Cone(double Radius, double Height)
+    //    {
+    //        this.Radius = Radius;
+    //        this.Height = Height;
+    //    }
+
+    //    public override double GetArea()
+    //    {
+    //        return PI * Radius * (Radius + Math.Sqrt(Height * Height + Radius * Radius));
+    //    }
+    //}
+
+    //class Program
+    //{
+    //    static void Main(string[] args)
+    //    {
+    //        Rectangle rectangle = new Rectangle(10, 20);
+    //        Console.WriteLine($"Area of Rectangle: {rectangle.GetArea()}");
+
+    //        Triangle triangle = new Triangle(15, 25);
+    //        Console.WriteLine($"Area of Triangle: {triangle.GetArea()}");
+
+    //        Circle circle = new Circle(12);
+    //        Console.WriteLine($"Area of Circle: {circle.GetArea()}");
+
+    //        Cone cone = new Cone(5, 15);
+    //        Console.WriteLine($"Area of Cone: {rectangle.GetArea()}");
+
+    //        Console.ReadKey();
+    //    }
+    //}
+    #endregion
+
+    #region Another example abour abstract class and abstract method
+    public abstract class Plan
     {
-        public void Add(int x, int y)
-        {
-            Console.WriteLine($"Addition of {x} and {y} is : {x + y}");
-        }
+        protected abstract double getRate();
 
-        public void Sub(int x, int y)
+        public void Calculation(int units)
         {
-            Console.WriteLine($"Subtraction of {x} and {y} is : {x - y}");
+            double rate = getRate();
+            Console.WriteLine($"Bill Amount For {units} Units is: Rs. {rate * units}");
         }
-
-        public abstract void Mul(int x, int y);
-        public abstract void Div(int x, int y);
     }
 
-    public class AbsChild : AbsParent
+    class CommercialPlan : Plan
     {
-        public override void Mul(int x, int y)
+        protected override double getRate()
         {
-            Console.WriteLine($"Multiplication of {x} and {y} is : {x * y}");
-        }
-
-        public override void Div(int x, int y)
-        {
-            Console.WriteLine($"Division of {x} and {y} is : {x / y}");
+            return 5.00;
         }
     }
 
-    class MainClass
+    class DomesticlPlan : Plan
+    {
+        protected override double getRate()
+        {
+            return 2.50;
+        }
+    }
+
+    class Program
     {
         static void Main(string[] args)
         {
-            // Error, we can't create an instance of an abstract class
-            //AbsParent parent = new AbsParent();
+            Plan p;
+            Console.WriteLine("Commercial Plan");
+            p = new CommercialPlan();
+            p.Calculation(250);
 
-            AbsChild absChild = new AbsChild();
-
-            // Creating abstract class reference pointing to child class object
-            AbsParent absParent = absChild;
-
-            absChild.Sub(1, 2);
-            absChild.Mul(1, 2);
-            absChild.Div(1, 2);
-            absChild.Add(1, 2);
-
-            Console.WriteLine();
-
-            absParent.Add(5, 10);
-            absParent.Sub(5, 10);
-            absParent.Div(5, 10);
-            absParent.Mul(5, 10);
+            Console.WriteLine("Domesticl Plan");
+            p = new DomesticlPlan();
+            p.Calculation(150);
+            Console.ReadKey();
         }
     }
     #endregion
--- Object Oriented Programming/Inheritance in C#/Program.cs
+++ Object Oriented Programming/Inheritance in C#/Program.cs
@@ -98,36 +98,321 @@
     #endregion
 
     #region How to pass dynamic value to Parent class constructor in C#?
-    class A
+    //class A
+    //{
+    //    public A(int number)
+    //    {
+    //        Console.WriteLine($"Class A Constructor is Called : {number}");
+    //    }
+    //    public void Method1()
+    //    {
+    //        Console.WriteLine("Method 1");
+    //    }
+    //    public void Method2()
+    //    {
+    //        Console.WriteLine("Method 2");
+    //    }
+    //}
+    //class B : A
+    //{
+    //    public B(int num) : base(num)
+    //    {
+    //        Console.WriteLine("Class B Constructor is Called");
+    //    }
+    //    public void Method3()
+    //    {
+    //        Console.WriteLine("Method 3");
+    //    }
+    //    static void Main()
+    //    {
+    //        B obj1 = new B(10);
+    //        B obj2 = new B(20);
+    //        B obj3 = new B(30);
+    //        Console.ReadKey();
+    //    }
+    //}
+    #endregion
+
+    #region Example to understand Single inheritance in C#
+    ////Parent class
+    //public class Rectangle
+    //{
+    //    public int length;
+    //    public int breadth;
+
+    //    public int Area()
+    //    {
+    //        return length * breadth;
+    //    }
+
+    //    public int Perimeter()
+    //    {
+    //        return 2 * (length + breadth);
+    //    }
+    //}
+
+    ////Child Class
+    //class Cuboid : Rectangle
+    //{
+    //    public int height;
+
+    //    public Cuboid(int l, int b, int h)
+    //    {
+    //        length = l;
+    //        breadth = b;
+    //        height = h;
+    //    }
+
+    //    public int Volume()
+    //    {
+    //        return length * breadth * height;
+    //    }
+    //}
+
+    //public class Program
+    //{
+    //    static void Main()
+    //    {
+    //        // Creating object of Child class and
+    //        // invoke the methods of Parent and Child classes
+    //        Cuboid obj = new Cuboid(2, 4, 6);
+
+    //        Console.WriteLine($"Volume is : {obj.Volume()}");
+    //        Console.WriteLine($"Area is : {obj.Area()}");
+    //        Console.WriteLine($"Perimeter is : {obj.Perimeter()}");
+    //        Console.ReadKey();
+    //    }
+    //}
+    #endregion
+
+    #region Example to Understand Multiple Inheritance in C# (Error example)
+    //public class Program
+    //{
+    //    static void Main()
+    //    {
+    //        // Creating object of Child class and
+    //        // invoke the methods of Parent classes and Child class
+    //        SmartPhone obj = new SmartPhone(); ;
+    //        obj.GetPhoneModel();
+    //        obj.GetCameraDetails();
+    //        obj.GetDetails();
+    //        Console.ReadKey();
+    //    }
+    //}
+    ////Parent Class 1
+    //class Phone
+    //{
+    //    public void GetPhoneModel()
+    //    {
+    //        Console.WriteLine("Redmi Note 5 Pro");
+    //    }
+    //}
+    ////Parent class2
+    //class Camera
+    //{
+    //    public void GetCameraDetails()
+    //    {
+    //        Console.WriteLine("24 Mega Pixel Camera");
+    //    }
+    //}
+    ////Child Class derived from more than one Parent class
+    //class SmartPhone : Phone, Camera // Compile Time error
+    //{
+    //    public void GetDetails()
+    //    {
+    //        Console.WriteLine("Its a RedMi Smart Phone");
+    //    }
+    //}
+    #endregion
+
+    #region Example to Understand IsA Relationship in C#
+    //class Rectangle
+    //{
+    //    //Data Members
+    //    public int Length;
+    //    public int Breadth;
+    //    //Member Functions
+    //    public int Area()
+    //    {
+    //        return Length * Breadth;
+    //    }
+    //    public int Perimeter()
+    //    {
+    //        return 2 * (Length + Breadth);
+    //    }
+    //}
+    ////Establishing Parent-Child Relationship
+    ////IS-A Relationship i.e. Cuboid IS-A Rectangle
+    //class Cuboid : Rectangle
+    //{
+    //    public int Height;
+    //    public Cuboid(int l, int b, int h)
+    //    {
+    //        Length = l;
+    //        Breadth = b;
+    //        Height = h;
+    //    }
+    //    public int Volume()
+    //    {
+    //        return Length * Breadth * Height;
+    //    }
+    //}
+
+    //public class Program
+    //{
+    //    static void Main()
+    //    {
+    //        Cuboid cuboid = new Cuboid(3, 5, 7);
+    //        Console.WriteLine($"Volume is : {cuboid.Volume()}");
+    //        Console.WriteLine($"Area is : {cuboid.Area()}");
+    //        Console.WriteLine($"Perimeter is : {cuboid.Perimeter()}");
+    //        Console.ReadKey();
+    //    }
+    //}
+    #endregion
+
+    #region Example to understand HasA relationship in C#
+    //class Address
+    //{
+    //    public string AddressLine, City, State;
+
+    //    public Address(string addressLine, string city, string state)
+    //    {
+    //        AddressLine = addressLine;
+    //        City = city;
+    //        State = state;
+    //    }
+    //}
+    //class Employee
+    //{
+    //    //Using Address in Employee class
+    //    //Establishing Has-A relationship i.e. Employee HAS-A Address   
+    //    public Address address;
+    //    public int Id;
+    //    public string Name;
+
+    //    public Employee(int id, string name, Address adrs)
+    //    {
+    //        Id = id;
+    //        Name = name;
+    //        address = adrs;
+    //    }
+
+    //    public void Display()
+    //    {
+    //        Console.WriteLine($"Employee Id: {Id}");
+    //        Console.WriteLine($"Employee Name: {Name}");
+    //        Console.WriteLine($"AddressLine: {address.AddressLine}");
+    //        Console.WriteLine($"City: {address.City}");
+    //        Console.WriteLine($"State: {address.State}");
+    //    }
+    //}
+
+    //public class Program
+    //{
+    //    static void Main()
+    //    {
+    //        Address address = new Address("B1-3029", "BBSR", "Odisha");
+    //        Employee employee = new Employee(1001, "Ramesh", address);
+    //        employee.Display();
+    //        Console.ReadKey();
+    //    }
+    //}
+    #endregion
+
+    #region Example to understand Specialization in C#
+    //class Rectangle
+    //{
+    //    public int Length;
+    //    public int Breadth;
+
+    //    public int Area()
+    //    {
+    //        return Length * Breadth;
+    //    }
+
+    //    public int Perimeter()
+    //    {
+    //        return 2 * (Length + Breadth);
+    //    }
+    //}
+
+    //class Cuboid : Rectangle
+    //{
+    //    public int Height;
+
+    //    public Cuboid(int l, int b, int h)
+    //    {
+    //        Length = l;
+    //        Breadth = b;
+    //        Height = h;
+    //    }
+
+    //    public int Volume()
+    //    {
+    //        return Length * Breadth * Height;
+    //    }
+    //}
+
+    //public class Program
+    //{
+    //    static void Main()
+    //    {
+    //        Cuboid cuboid = new Cuboid(3, 5, 7);
+    //        Console.WriteLine($"Volume is : {cuboid.Volume()}");
+    //        Console.WriteLine($"Area is : {cuboid.Area()}");
+    //        Console.WriteLine($"Perimeter is : {cuboid.Perimeter()}");
+
+    //        Console.ReadKey();
+    //    }
+    //}
+    #endregion
+
+    #region Example to Understand Generalization in C#
+    interface ICar
     {
-        public A(int number)
+        void Start();
+        void Stop();
+    }
+
+    public class Innova : ICar
+    {
+        public void Start()
         {
-            Console.WriteLine($"Class A Constructor is Called : {number}");
+            Console.WriteLine($"Innova Start");
         }
-        public void Method1()
+
+        public void Stop()
         {
-            Console.WriteLine("Method 1");
-        }
-        public void Method2()
-        {
-            Console.WriteLine("Method 2");
+            Console.WriteLine($"Innova Stop");
         }
     }
-    class B : A
+
+    public class BMW : ICar
     {
-        public B(int num) : base(num)
+        public void Start()
         {
-            Console.WriteLine("Class B Constructor is Called");
+            Console.WriteLine($"BMW Start");
         }
-        public void Method3()
+
+        public void Stop()
         {
-            Console.WriteLine("Method 3");
+            Console.WriteLine($"BMW Stop");
         }
+    }
+
+    public class Program
+    {
         static void Main()
         {
-            B obj1 = new B(10);
-            B obj2 = new B(20);
-            B obj3 = new B(30);
+            Innova innova = new Innova();
+            innova.Start();
+            innova.Stop();
+
+            ICar bmw = new BMW();
+            bmw.Start();
+            bmw.Stop();
+
             Console.ReadKey();
         }
     }
